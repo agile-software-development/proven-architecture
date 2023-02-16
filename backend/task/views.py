a7b365508af56b22cae4436c1a3cd6990e8ec8ba
Danial Erfanian <erfaniiyanerfan@gmail.com>
2023-02-16 22:57:26 +0330

    Add backend project

---
+++ backend/task/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+
+from task.models import Task
+from task.serializers import TaskSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# Create your views here.
